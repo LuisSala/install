c2558ec66f76ae7b3910cf3447d169e87ff80b7b
Luis Sala <lsala@primogen.com>
2016-09-04 19:26:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ sudo pip install awscli
 echo "Installing CLI Tools"
 git clone https://github.com/LuisSala/c9-shell-scripts ~/bin
 
-echo "Switching Branch"
-cd ~/workspace
-git checkout BoxWorks
-
 echo "Install Complete"
 echo "Please run the following command to load new settings:"
 echo "source ~/.profile"
