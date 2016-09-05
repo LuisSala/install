2e37063fcfb72f3cd5384802605befbae739f2b0
Luis Sala <lsala@primogen.com>
2016-09-04 19:08:10 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #! /bin/bash
 
-cd ~
-
 echo "Installing AWS CLI"
 sudo pip install awscli
 
 echo "Installing CLI Tools"
-git clone https://github.com/LuisSala/c9-shell-scripts ./bin
-
-echo "Configuring Workspace"
-mkdir ~/.workspace-backup
-mv ~/workspace/* ~/.workspace-backup
-
-echo "Cloning Project"
-git clone https://github.com/LuisSala/aws-lambda-zombie-workshop .
+git clone https://github.com/LuisSala/c9-shell-scripts ~/bin
 
 echo "Switching Branch"
+cd ~/workspace
 git checkout BoxWorks
 
 echo "Install Complete"
